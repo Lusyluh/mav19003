bb0244629845fd2ee7419b417e3c8fce91492a05
Lusyluh <mav19003@byui.edu>
2022-01-09 07:22:35 +0200

    updated javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-const weeklyList = [
-    {label:'WEEK 1',url: 'week1/indext.html'}
-]
-append('tableOfContents', weeklyList)
+
+const links = [
+    {label:'WEEK1',
+    url: 'week1/index.html'},
+    {label:'WEEK2',
+    url: 'week2/index.html'},
+    {label:'WEEK3',
+    url: 'week3/index.html'},
+    {label:'WEEK 4',
+    url: 'week4/index.html'},
+    {label:'WEEK 5',
+    url: 'week5/index.html'}
+
+];
+append('tc', links);
+
+function append(listId, arr){
+
+    let ol = document.getElementById(listId);
+
+    for (let i = 0; i<arr.length; i++){
+
+        let li = document.createElement("LI");
+        let a = document.createElement("A");
+
+        a.setAttribute('href', arr[0].url);
+        a.innerHTML = arr[0].label;
+
+        ol.appendChild(li);
+        li.appendChild(a);
+        
+    }
+}
