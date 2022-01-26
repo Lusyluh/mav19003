cfaeaa395b53b79ff81cf7ef292d7f67a6c624a2
Lusyluh <mav19003@byui.edu>
2022-01-26 02:38:43 +0200

    week3activities

--- week 3/activities/Javascript/readings.js
+++ week 3/activities/Javascript/readings.js
@@ -1,10 +1,30 @@
-let  user = {
-   name: "Lusanda",
-    age: "21",
+// let  user = {
+//    name: "Lusanda",
+//     age: "21",
 
 
-sayHi(){
-    alert(this.name);
-}
+// sayHi(){
+//     alert(this.name);
+// }
+// };
+// user.sayHi();
+
+
+//exercise---create a calculator
+
+let calculator = {
+    read(){
+        this.a = +prompt('a?',0);
+        this.b = +prompt('b?',0);
+
+    },
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a * this.b;
+    }
 };
-user.sayHi();
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
