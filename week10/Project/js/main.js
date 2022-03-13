835404706a12f93b709d0caa230afeb625d27333
Lusyluh <mav19003@byui.edu>
2022-03-13 02:38:51 +0200

    fetch api data

--- week10/Project/js/main.js
+++ week10/Project/js/main.js
@@ -1,15 +1,17 @@
 //get the api key
 const myKey = 'cbd2cdfdd1f438f29af3417a193151ae';
-//const inputVal = input.value;
+const inputval = document.querySelector('#cityinput').value;
+const btn = document.querySelector('#add');
 //connect to openweathermap.com
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=${myKey}&units=metric`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputval}&appid=${myKey}&units=metric`;
 
 //fetch data and return json
-fetch(apiURL)
+btn.addEventListener('click', function(){
+  fetch(apiURL)
   .then(response => response.json())
   .then(jsObject => {
     console.log(jsObject);
 })
 //.catch(() => {
   //msg.textContent = "Please search for a valid city 😩";
-//});
+});
