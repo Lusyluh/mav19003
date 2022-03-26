6a7e54f30e0124055bd2f4f4b00c777cd377fc16
Lusyluh <mav19003@byui.edu>
2022-03-26 02:55:43 +0200

    read data

--- week10/Project/js/main.js
+++ week10/Project/js/main.js
@@ -3,6 +3,7 @@
 const inputval = document.querySelector('#cityinput');
 const inputValue = inputval.value;
 const btn = document.querySelector('#add');
+const msg = document.querySelector('.err-msg')
 //connect to openweathermap.com
 
 
@@ -13,10 +14,47 @@ btn.addEventListener('click', e =>{
   const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=cbd2cdfdd1f438f29af3417a193151ae&units=imperial`;
   fetch(apiURL)
   .then(response => response.json())
-  .then(data => console.log(data))
-  //.then(jsObject => {
-    //console.log(jsObject);
-//})
-//.catch(() => {
-  //msg.textContent = "Please search for a valid city 😩";
-});
+  .then(data =>{
+    console.log(data);
+    displayResults(data)
+    //document.getElementById('temp').textContent = data.main.temp;
+    //let myCity = document.querySelector('.location .myCity');
+    //myCity.innerText = `${data.name}, ${data.sys.country}`;
+    })
+  
+  .catch(() => {
+    msg.textContent = "Please search for a valid city";})
+  })
+//data to fetch and display
+const displayResults = (weather) =>{
+  let myCity = document.querySelector('.location .myCity');
+  myCity.innerText = `${weather.name}, ${weather.sys.country}`;
+  let temperature = document.getElementById('temp');
+  temperature.textContent = weather.main.temp;
+  //get the current date and display it as Day Month Date and year
+  let todayDate = new Date();
+  let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(todayDate);
+
+  //get the icon associated with the current weather
+  let imagesrc = 'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png';
+  document.getElementById('icon').setAttribute('src',imagesrc);
+  document.getElementById('icon').setAttribute('alt',desc);
+
+  //get the weather and weather description
+  let currentWeather = document.querySelector('.current .weather');
+  currentWeather.textContent = weather.weather[0].main;
+  let desc = document.querySelector('.desc');
+  desc.innerText = weather.weather[0].description;
+  
+}
+// returns today's date
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+  return `${day}, ${date} ${month} ${year}`;
+}
